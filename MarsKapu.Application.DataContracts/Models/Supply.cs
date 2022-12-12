f0d99a98994f36fb08203a83ad8e3063744d4ca9
Ádám Sipos <siposadam02@gmail.com>
2022-12-12 16:00:46 +0100

    supplies

--- MarsKapu.Application.DataContracts/Models/Supply.cs
+++ MarsKapu.Application.DataContracts/Models/Supply.cs
@@ -8,5 +8,24 @@ namespace MarsKapu.DataContracts.Models
 {
     public class Supply
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime BestByDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+
+        public Supply(int id, string name, int quantity, DateTime bestbydate, DateTime shippingdate)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            BestByDate = bestbydate;
+            ShippingDate = shippingdate;
+        }
+
+        public override string ToString()
+        {
+            return Id+";"+Name+";"+Quantity+";"+BestByDate.ToString()+";"+ShippingDate.ToString();
+        }
     }
 }
--- MarsKapu.Services/SupplyService.cs
+++ MarsKapu.Services/SupplyService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MarsKapu.Services
 {
@@ -12,7 +13,25 @@ namespace MarsKapu.Services
     {
         public void AddSupply(Supply supply)
         {
-            throw new NotImplementedException();
+            List<Supply> supplies = GetSupplyInventory();
+            int index = 0;
+            while (index < supplies.Count && supplies[index].Id != supply.Id)
+            {
+                index++;
+            }
+            if (index == supplies.Count) supplies.Add(supply);
+            else
+            {
+                supplies[index].Quantity += supply.Quantity;
+                supplies[index].BestByDate = supply.BestByDate;
+                supplies[index].ShippingDate = supply.ShippingDate;
+            }
+            List<string> output = new List<string>();
+            foreach (var item in supplies)
+            {
+                output.Add(item.ToString());
+            }
+            File.WriteAllLines("Repositories/DataBase/Supplies.txt", output);
         }
 
         public void AddSupplyList(List<Supply> supplies)
@@ -22,7 +41,22 @@ namespace MarsKapu.Services
 
         public List<Supply> GetSupplyInventory()
         {
-            throw new NotImplementedException();
+            List<Supply> supplies = new List<Supply>();
+            using (StreamReader r = new StreamReader("Repositories/DataBase/Supplies.txt"))
+            {
+                while (r.EndOfStream == false)
+                {
+                    string line = r.ReadLine();
+                    var data = line.Split(";");
+                    int id = int.Parse(data[0]);
+                    string name = data[1];
+                    int quantity = int.Parse(data[2]);
+                    DateTime bestbydate = DateTime.Parse(data[3]);
+                    DateTime shippingdate = DateTime.Parse(data[4]);
+                    supplies.Add(new Supply(id, name, quantity, bestbydate, shippingdate));
+                }
+            }
+            return supplies;
         }
 
         public void RequestSupplies(List<Supply> supplies)
