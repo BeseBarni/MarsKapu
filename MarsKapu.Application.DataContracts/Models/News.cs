9c56ed71b46c8ea4060a2735fc9ca51f99960af1
GLaura22 <laura.gergelyi@gmail.com>
2022-12-12 14:48:39 +0100

    hirlekerdezes

--- MarsKapu.Application.DataContracts/Models/News.cs
+++ MarsKapu.Application.DataContracts/Models/News.cs
@@ -8,8 +8,20 @@ namespace MarsKapu.DataContracts.Models
 {
     public class News
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+
         public bool Approved { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public News(int id, string title, string text, bool approved, DateTime publishDate)
+        {
+            Id = id;
+            Title = title;
+            Text = text;
+            Approved = approved;
+            PublishDate = publishDate;
+        }
     }
 }
